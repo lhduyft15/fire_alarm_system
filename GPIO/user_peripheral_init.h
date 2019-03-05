efb6f05b0c60fdb8e19f55f6e3b1ed80b6f442f2
Hue_Gengar <huekh70@gmail.com>
2019-03-05 14:09:42 +0700

    Add files via upload

---
+++ GPIO/user_peripheral_init.h
@@ -0,0 +1,21 @@
+/*
+ * user_peripheral_init.h
+ *
+ *  Created on: Mar 4, 2019
+ *      Author: huekh
+ */
+
+#ifndef USER_FILES_USER_PERIPHERAL_INIT_H_
+#define USER_FILES_USER_PERIPHERAL_INIT_H_
+
+//Define port and pin for led
+#define LED_PORT 		(gpioPortF)
+#define LED_0_PIN 		(4U)
+#define LED_1_PIN 		(5U)
+
+//Define port and pin for button
+#define BUTTON_PORT 	(gpioPortF)
+#define BUTTON_0_PIN 	(6U)
+#define BUTTON_1_PIN 	(7U)
+
+#endif /* USER_FILES_USER_PERIPHERAL_INIT_H_ */
